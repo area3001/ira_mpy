705f5fb97e12be074d71ababf2ed124bb57fcbe6
Daan Gerits <daan.gerits@gmail.com>
2024-02-21 23:28:41 +0100

    working version with nats. still issues with driving neopixels

--- ira.py
+++ ira.py
@@ -3,27 +3,45 @@ import network
 import json
 import ubinascii
 
-import mpynats as nats
+import nats
+
 
 class Ira:
-    def __init__(self, id, name, hw_type, hw_version, url='nats://demo.nats.io:4222'):
+    def __init__(self, id, name, hw_type, hw_version, group="default", url='nats://demo.nats.io:4222'):
         self.id = id
         self.name = name
+        self.group = group
         self.wlan = network.WLAN(network.STA_IF)
         self.hw_type = hw_type
         self.hw_version = hw_version
+        self.mode = 0
         
         self.c = nats.Connection('nats://demo.nats.io:4222')
+        self.ht = None
+        self.handlers = {}
+        
+    def close(self):
+        self.done.set()
+        
+    def register_handler(self, command, cb):
+        self.handlers[command] = cb
         
     async def listen(self):
         self.c.connect()
-        asyncio.run(self._heartbeat_loop())
+        asyncio.create_task(self._heartbeat_loop())
         print('nats server connected')
+
+        self.c.subscribe('area3001.ira.{}.devices.{}.output'.format(self.group, self.id), self._parse_message)
+        self.c.subscribe('area3001.ira.{}.output'.format(self.group), self._parse_message)
+        
+        self.ht = asyncio.create_task(self.c.wait())
+        await self.ht
+        
     
     async def _heartbeat_loop(self):
         print('heartbeat')
         while True:
-            self.c.publish('area3001.ira.{}'.format(self.id), self._heartbeat_msg())
+            self.c.publish('area3001.ira.{}.devices.{}'.format(self.group, self.id), self._heartbeat_msg())
             print('sent heartbeat')
             await asyncio.sleep_ms(30_000)
             
@@ -31,6 +49,7 @@ class Ira:
         return json.dumps({
             'id': self.id,
             'name': self.name,
+            'group': self.group,
             'hardware': {
                 'kind': self.hw_type,
                 'version': self.hw_version,
@@ -40,3 +59,17 @@ class Ira:
                 'ip': self.wlan.ifconfig()[0]
             }
         })
+    
+    def _parse_message(self, msg):
+        try:
+            data = msg.data.split()
+            if len(data) == 0:
+                return
+            
+            if data[0] in self.handlers:
+                self.handlers[data[0]](data)
+            else:
+                raise ValueError('unknown command %s' % data[0])
+            
+        except Exception as t:
+            print('failed to process message \"%s\": %s' % (msg.data, t))
--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import asyncio
 import network
 from ira import Ira
 
+from machine import Pin
+from neopixel import NeoPixel
+
+pin = Pin(2, Pin.OUT)   # set GPIO0 to output to drive NeoPixels
+np = NeoPixel(pin, 16)   # create NeoPixel driver on GPIO0 for 8 pixels
+#np[0] = (255, 255, 255) # set the first pixel to white
+#np.write()              # write data to all pixels
+
+
+# nats publish -s nats://demo.nats.io:4222 area3001.ira.default.output 'set_pixel 0 #ff0000'
+def set_neopixel_rgb(data):
+    color = data[2].lstrip('#')
+    color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+    np[int(data[1])] = (0, 0, 255)
+    np.write()
+    
+    print('setting neopixel %d to %s' % (int(data[1]), color))
+
 async def main():
     import network
     sta_if = network.WLAN(network.STA_IF)
@@ -16,6 +34,8 @@ async def main():
     print('network config:', sta_if.ifconfig())
 
     i = Ira('my_id', 'my_name', 'ira', '2024')
+    i.register_handler('set_pixel', set_neopixel_rgb)
+    
     await i.listen()
     print('listening to ira messages')
 
