51b6bd891983aad49de94e4f3d79e43a50422a20
Hans Polders <hans.polders@gmail.com>
2024-08-15 15:35:03 +0200

    Rainbow update

--- firmware/fx/rainbow.py
+++ firmware/fx/rainbow.py
@@ -27,17 +27,18 @@ def wheel(index:int, brightness:int=255) -> tuple(int,int,int):
 ## make sure the output exists and is passed as a string
 ##
 async def run(device, config):
-    print(config)
-    print("running rainbow fx on", config)
+    print("running rainbow fx on")
 
     max_length = 0    
     devices = []
     for out_id in device.outputs:
-        devices.append((device.outputs[out_id],device.outputs[out_id].rgb_length(),360/device.outputs[out_id].rgb_length()))
-        max_length = max(max_length,device.outputs[out_id].rgb_length())
+        _device = device.outputs[out_id]
+        _device_length = device.outputs[out_id].rgb_length()
+        _device_gap = 360/device.outputs[out_id].rgb_length()
+        devices.append((_device, _device_length, _device_gap))
+        max_length = max(max_length,_device_length)
 
     brightness = 128
-    gap=3
     if "brightness" in config and type(config["brightness"]) == int:
         brightness = config["brightness"]
 
@@ -46,12 +47,13 @@ async def run(device, config):
         do_sparkle = False
     
     while True:
-        for j in range(max_length):
-            for device in devices:
-                for i in range(device[1]):
-                    device[0].rgb_set(i, wheel(int((i+j)*gap), brightness))
+        for j in range(0,360,3):
+            for _device in devices:
+                for i in range(_device[1]):
+                    _device[0].rgb_set(i, wheel(int(i*_device[2])+j, brightness))
                 if do_sparkle and j % 5 == 0:
                     device[0].rgb_set(random.randint(0,device[1]-1),(255,255,255))
-                device[0].rgb_write()
+                _device[0].rgb_write()
             await asyncio.sleep_ms(33)
+        
         
